8a20f09993acf1243a8b5f20c3db12d6ccf93b84
maxwen <max.weninger@gmail.com>
2021-10-22 14:55:26 +0200

    [SQUASH]base: Add OmniJawsClient
    neobuddy89:
    Cleaned up version from https://github.com/omnirom/android_packages_apps_OmniLib
    Keeping it accessible for weather client packages as well as SystemUI package.
    
    Change-Id: Ifadfaa7f4438e3bde52f116f002a7a5273790184
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Signed-off-by: Mr- 0 <oldcrocs21@gmail.com>
    
    OmniJaws: add force refresh hook
    
    Send ContentValues update=true to url content://org.omnirom.omnijaws.provider/control
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Signed-off-by: Mr- 0 <oldcrocs21@gmail.com>
    
    OmniJaws: Use google icon set by default
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Signed-off-by: Mr- 0 <oldcrocs21@gmail.com>
    
    SystemUI: add OmniJaws keyguard slice provider
    
    Change-Id: Ic74dc968bde4efa4560c3d56df4f7d8c9244793d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Mr- 0 <oldcrocs21@gmail.com>
    
    SystemUI: add enablement settings for OmniJaws keyguard slice provider
    
    Change-Id: Ic3a386fae8ca63708628cd0b5d5dfe359743e539
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Signed-off-by: Mr- 0 <oldcrocs21@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5809,6 +5809,11 @@ public final class Settings {
          */
         public static final String UDFPS_ICON = "udfps_icon";
 
+        /**
+         * @hide
+         */
+        public static final String LOCKSCREEN_WEATHER_ENABLED = "lockscreen_weather_enabled";
+
        /**
          * These are all public system settings
          *
