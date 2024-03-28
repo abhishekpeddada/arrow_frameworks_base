59cacce5ec6d02114e84f487d0855e27640478ec
abhishekpeddada <abhishekpeddada@gmail.com>
2024-03-28 13:49:24 +0000

    Revert "Introduce Lockscreen Style [1/2]"
    This reverts commit 0f71c324dd59bcc79d76c4100497b6db7900e49e.

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -225,6 +225,5 @@ public class SecureSettings {
         Settings.Secure.ACCESSIBILITY_FONT_SCALING_HAS_BEEN_CHANGED,
         Settings.Secure.TETHERING_ALLOW_VPN_UPSTREAMS,
         Settings.Secure.DOZE_ON_CHARGE
-        Settings.Secure.CLOCK_LS
     };
 }
